265ee4ba3131594c96cf890202b34fb8a69a222b
zxp <zxp@1mdata.com>
2017-10-04 10:59:41 +0800

    export all

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var MongoDB = require('./lib/mongo/MongoDB');
+var MongoDao = require('./lib/mongo/MongoDao');
 var MysqlDB = require('./lib/mysql/MysqlDB');
+var MysqlDao = require('./lib/mysql/MysqlDao');
 var api = require('./lib/apiResult');
 var Redis = require('./lib/Redis');
 var errors = require('./lib/errors');
@@ -15,7 +17,12 @@ var enuo = {
     api: api,
     redis: new Redis(),
     errors: errors,
-    logger: logger
+    logger: logger,
+    MongoDB: MongoDB,
+    MysqlDB: MysqlDB,
+    Redis: Redis,
+    MongoDao: MongoDao,
+    MysqlDao: MysqlDao
 };
 
 module.exports = enuo;
