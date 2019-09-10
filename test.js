fed8210a65ba61b610c26e9b6441daa5199b65ce
zxp <wszxp1234@126.com>
2019-09-10 14:58:16 +0800

    add log4js-mongo-appender

--- test.js
+++ test.js
@@ -1,32 +1,25 @@
 var core = require('./index');
 var logger = core.logger;
 
-// var otp = logger.default;
-// otp.appenders.logstash = {
-//     type: 'log4js-logstash-tcp',
-//     host: '10.10.10.16',
-//     port: 5678,
-// };
-// otp.appenders.filterLogstash = {
-//     "type": "logLevelFilter",
-//     "level": "ERROR",
-//     "appender": 'logstash'
-// };
-// otp.categories.default.appenders.push('filterLogstash');
-// var fields = {
+// 1. default 
+// is write to file (./logs) and console
+
+
+// 2. logstash  
+// ======================== yarn add log4js-logstash-tcp ==================================
+// var otp = logger.logstash('10.10.10.16', 5678);
+// logger.configure('myapp', otp, {
 //     "source": "source1",
 //     "env": "dev"
-// };
-// logger.configure('myapp', otp, fields);
+// });
+// core.logger.error('error test3!');
 
-//"log4js-logstash-tcp": "^1.0.8",
 
-var otp = logger.logstash('10.10.10.16', 5678);
-logger.configure('myapp', otp, {
-    "source": "source1",
-    "env": "dev"
-});
-
-core.logger.error('error test2!', 'test info 1', {
-    info2: 'info2'
-});
+// 3. mongo 
+// ======================== yarn add log4js-mongo-appender ==================================
+// var otp = logger.mongodb('127.0.0.1', 27017, 'logs', 'log4js_logs');
+// logger.configure('test-mongo', otp, {
+//     "source": "source1",
+//     "env": "dev"
+// });
+// core.logger.error('error test4!',{a:'b'});
